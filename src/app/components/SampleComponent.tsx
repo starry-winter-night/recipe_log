7a050333ffca48c92773719de8089242e698ed36
smpark <smpark7723@gmail.com>
2024-05-03 23:38:23 +0900

    Delete unused files

--- src/app/components/SampleComponent.tsx
+++
@@ -1,3 +0,0 @@
-export default function SampleComponent() {
-  return <div>Hello, Next.js with Jest!</div>;
-}
--- src/test/unit/component/SampleComponent.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import SampleComponent from '@/app/components/SampleComponent';
-
-describe('SampleComponent', () => {
-  it('renders the text correctly', () => {
-    render(<SampleComponent />);
-    expect(screen.getByText('Hello, Next.js with Jest!')).toBeInTheDocument();
-  });
-});
